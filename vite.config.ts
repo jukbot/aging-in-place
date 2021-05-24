421d8e028222043faa5bbfca646cae78020f2d83
Chukkrit Visitsaktavorn <jukbot@yellotalk.co>
2021-05-25 05:27:16 +0700

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactJsx from 'vite-react-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), reactJsx()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
